44c19682b0500c4c3b1c7749aa17cafd03aa0be7
TomasKimer <tomas.kimer@gmail.com>
2015-03-20 11:09:16 +0100

    Base structure of project.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public  static GameController Instance { get { return m_Instance; } private set { m_Instance = value; } }
+    private static GameController m_Instance;
+
+    void Awake() {
+        Debug.Log("GameController Init");
+
+        m_Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
