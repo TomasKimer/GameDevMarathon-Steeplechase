0b5323b383585844e078957414aeb38f75010ba1
TomasKimer <tomas.kimer@gmail.com>
2015-03-20 11:45:55 +0100

    base avatar input

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Avatar : MonoBehaviour {
 
+    public float m_MoveSpeed = 10.0f;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,24 @@ public class Avatar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
+        Vector3 moveVec = new Vector3();
+         
+        if (Input.GetKey(KeyCode.W)) {
+            moveVec.y = 1f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            moveVec.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            moveVec.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveVec.x = 1;
+        }
+
+        Transform tr = GetComponent<Transform>();
+        tr.Translate(moveVec * Time.deltaTime * m_MoveSpeed);
+
 	}
 }
