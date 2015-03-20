14f598fc137e2bd74f587699723c244d1b273525
TomasKimer <tomas.kimer@gmail.com>
2015-03-20 14:01:47 +0100

    avatar facing to move direction

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -3,34 +3,37 @@ using System.Collections;
 
 public class Avatar : MonoBehaviour {
 
-    public float m_MoveSpeed = 10.0f;
+    public float moveSpeed = 10.0f;
 
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-        Vector3 moveVec = new Vector3();
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+        Vector3 direction = new Vector3();
          
-        if (Input.GetKey(KeyCode.W)) {
-            moveVec.y = 1f;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            direction.y = 1f;
         }
-        if (Input.GetKey(KeyCode.S)) {
-            moveVec.y = -1f;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            direction.y = -1f;
         }
-        if (Input.GetKey(KeyCode.A)) {
-            moveVec.x = -1;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            direction.x = -1;
         }
-        if (Input.GetKey(KeyCode.D)) {
-            moveVec.x = 1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            direction.x = 1;
         }
 
-        Transform tr = GetComponent<Transform>();
-        tr.Translate(moveVec * Time.deltaTime * m_MoveSpeed);
+        if (direction.x != 0f || direction.y != 0f) {
+            Transform tr = GetComponent<Transform>();
+            tr.position += direction.normalized * Time.deltaTime * moveSpeed;
 
-	}
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            tr.localRotation = Quaternion.AngleAxis(angle + 180, Vector3.forward);
+        }
+    }
 }
