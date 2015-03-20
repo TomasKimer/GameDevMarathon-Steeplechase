b1b51616b5258408e347cafd3c179cf9659123c8
burdas07 <burdas07@gmail.com>
2015-03-20 14:13:56 +0100

    Generovani zdi
    asfasdfsdasdfa

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+
+	public GameObject wallPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+		InstantiateWall (-15, 15);
+		InstantiateWall (-15, 15);
+		InstantiateWall (-15, 15);
+		InstantiateWall (-15, 15);
+		InstantiateWall (-15, 15);
+
+		wallPrefab.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InstantiateWall(int minPos, int maxPos){
+	
+		Instantiate(wallPrefab, new Vector3(Random.Range(minPos, maxPos), Random.Range(minPos, maxPos), 0), 
+		            Quaternion.Euler (0, 0, (Random.Range(0, 180))));
+
+	
+	}
+}
