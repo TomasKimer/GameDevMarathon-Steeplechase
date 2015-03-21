6f19b6450db2a9ba04fb33d566550b5a6aa30b10
p4jos <pavelcsoka@dhcps132.fit.vutbr.cz>
2015-03-21 01:24:24 +0100

    horizontal camera boundaries added

--- Assets/Scripts/BloodMarks.cs
+++ Assets/Scripts/BloodMarks.cs
@@ -10,11 +10,17 @@ public class BloodMarks : MonoBehaviour
 
 	// Use this for initialization
 	void Start ()
-	{	
+	{
+		Instantiate (bloodDie, new Vector3 (), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{	
 	}
+
+//	Vector3 genPosition ()
+//	{
+//
+//	}
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 	
 	public int movementSpeed = 3;
 	public int rotationAngle = 15;
 	public int rotationSpeed = 10;
 	public static float[] verticalBounds = {10f, -10f};
+	public static float[] horizontalBounds = {280f, -280f};
 
 	private Quaternion originalRotation;
 	private float offsetZ = -10f;
@@ -17,61 +19,68 @@ public class CameraController : MonoBehaviour {
 	private int[] randomLevelsY = new int[] {-1, 1};
 
 	// Use this for initialization
-	void Start(){
-		this.transform.position = new Vector3(0, 0, offsetZ);
-		transform.rotation = Quaternion.Euler(0, 0, 0);
+	void Start ()
+	{
+		this.transform.position = new Vector3 (0, 0, offsetZ);
+		transform.rotation = Quaternion.Euler (0, 0, 0);
 
 		originalRotation = transform.rotation;
 
-		StartCoroutine("changeDirection");
-		StartCoroutine("shakeCam");
+		StartCoroutine ("changeDirection");
+		StartCoroutine ("shakeCam");
 
-        Game.Instance.GameRestart += OnGameRestart;
+		Game.Instance.GameRestart += OnGameRestart;
 	}
 
-    void OnDestroy() {
-        if (Game.Instance != null)
-            Game.Instance.GameRestart -= OnGameRestart;
-    }
+	void OnDestroy ()
+	{
+		if (Game.Instance != null)
+			Game.Instance.GameRestart -= OnGameRestart;
+	}
 
-    void OnGameRestart() { 
-        this.transform.position = new Vector3(0, 0, offsetZ);
+	void OnGameRestart ()
+	{ 
+		this.transform.position = new Vector3 (0, 0, offsetZ);
 		transform.rotation = Quaternion.identity;
 		originalRotation = transform.rotation;
 
-        StopAllCoroutines();
-        StartCoroutine("changeDirection");
-        StartCoroutine("shakeCam");
-    }
+		StopAllCoroutines ();
+		StartCoroutine ("changeDirection");
+		StartCoroutine ("shakeCam");
+	}
 
-    void OnEnable() {
+	void OnEnable ()
+	{
 		//StartCoroutine("changeDirection");
 		//StartCoroutine("shakeCam");
-    }
+	}
 
-    void OnDisable() {
-        //StopAllCoroutines();
-    }
+	void OnDisable ()
+	{
+		//StopAllCoroutines();
+	}
 	
-	IEnumerator changeDirection() {
-		for(;;) {
-			movementTranslation.x = randomLevelsX[Random.Range(0, randomLevelsX.Length)];
-			movementTranslation.y = randomLevelsY[Random.Range(0, randomLevelsY.Length)];
+	IEnumerator changeDirection ()
+	{
+		for (;;) {
+			movementTranslation.x = randomLevelsX [Random.Range (0, randomLevelsX.Length)];
+			movementTranslation.y = randomLevelsY [Random.Range (0, randomLevelsY.Length)];
 			//movementTranslation.z = ((int)Random.Range(0, 2));
-			yield return new WaitForSeconds(cameraChangeSeconds); 
+			yield return new WaitForSeconds (cameraChangeSeconds); 
 		}
 	}
 	
-	IEnumerator shakeCam() {
+	IEnumerator shakeCam ()
+	{
 		//int rotation = rotationAngle;
 		int negRotation = -1 * rotationAngle;
 		float deltaRotation = 0;
 		//int speed = rotationSpeed;
 		bool forward = true;
-		for(;;) {
-			if(deltaRotation < rotationAngle && forward) {
+		for (;;) {
+			if (deltaRotation < rotationAngle && forward) {
 				deltaRotation += rotationSpeed * Time.deltaTime;
-			} else if(deltaRotation >= negRotation){
+			} else if (deltaRotation >= negRotation) {
 				forward = false;
 				deltaRotation -= rotationSpeed * Time.deltaTime;
 			} else {
@@ -79,7 +88,7 @@ public class CameraController : MonoBehaviour {
 				yield return null;
 			}
 
-			transform.rotation = Quaternion.Euler(originalRotation.eulerAngles.x, originalRotation.eulerAngles.y,
+			transform.rotation = Quaternion.Euler (originalRotation.eulerAngles.x, originalRotation.eulerAngles.y,
 			                                      originalRotation.eulerAngles.z + deltaRotation);
 
 			yield return null;
@@ -87,13 +96,18 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update(){
+	void Update ()
+	{
 		float newPositionY = transform.position.y + movementTranslation.y;
 
-		if(newPositionY >= verticalBounds[0] || newPositionY <= verticalBounds[1]) {
+		if (newPositionY >= verticalBounds [0] || newPositionY <= verticalBounds [1]) {
 			movementTranslation.y = 0;
 		}
 
-		transform.Translate(movementTranslation * Time.deltaTime * movementSpeed, Space.World); 
+		if (transform.position.x >= horizontalBounds [0] || transform.position.x <= horizontalBounds [1]) {
+			movementSpeed *= -1;
+		}
+
+		transform.Translate (movementTranslation * Time.deltaTime * movementSpeed, Space.World); 
 	}
 }
