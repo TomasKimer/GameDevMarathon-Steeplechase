0aa3b10de44c7e00a5ea334701a702739038bde7
p4jos <pavelcsoka@dhcps132.fit.vutbr.cz>
2015-03-21 03:20:25 +0100

    z-coodinate of walls repaired

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Level : MonoBehaviour {
+public class Level : MonoBehaviour
+{
 
 	private const float groundTileSize = 100;
 	private const int groundArea = 2;
@@ -17,46 +18,51 @@ public class Level : MonoBehaviour {
 
 	
 	[Range(0.0f, 1.0f)]
-    public float tintInterval = 0.12f;
+	public float
+		tintInterval = 0.12f;
 
-    private SpriteRenderer[] m_groundSprites;
+	private SpriteRenderer[] m_groundSprites;
 
-    void Awake() {
+	void Awake ()
+	{
 
-    }
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 		CreateFloor ();
 		CreateWalls ();
 		CreatePowerUps ();
 
-        //Game.Instance.PowerupManager.GeneratePowerup(PowerupManager.E_PowerupType.PowerDownJump, Vector3.zero, Quaternion.identity);
+		//Game.Instance.PowerupManager.GeneratePowerup(PowerupManager.E_PowerupType.PowerDownJump, Vector3.zero, Quaternion.identity);
 
 		//wallPrefab.SetActive (false);
 
-        m_groundSprites = GetComponentsInChildren<SpriteRenderer>();
+		m_groundSprites = GetComponentsInChildren<SpriteRenderer> ();
         
-        StartCoroutine( _TintRepeated() );
+		StartCoroutine (_TintRepeated ());
 	}
 
 	/**
 	 * Puts all the floor tiles in the scene
 	 */
-	void CreateFloor(){
+	void CreateFloor ()
+	{
 		int areaOffset = -55;
 
 		for (int j = -groundArea-1; j < groundArea+1; j++) {
 			GameObject ground = Instantiate (groundPrefab, new Vector3 (j * groundTileSize, groundTileSize + areaOffset, 10), 
 				                                 Quaternion.identity) as GameObject;
-                ground.transform.parent = transform;
+			ground.transform.parent = transform;
 		}
 	}
 	/**
 	 * Generates walls on the floor area
 	 */
-	void CreateWalls (){
+	void CreateWalls ()
+	{
 
 		int screenOffset = 17;
 
@@ -73,18 +79,18 @@ public class Level : MonoBehaviour {
 				float xcoord = 0;
 				float ycoord = 0; 
 
-				for(int k=0; k<=100; k+=10){
-					for(int l=0; l<=40; l+=10){
+				for (int k=0; k<=100; k+=10) {
+					for (int l=0; l<=40; l+=10) {
 
-						xOffset = Random.Range(-3f, 3f);
-						yOffset = Random.Range(-3f, 3f);
+						xOffset = Random.Range (-3f, 3f);
+						yOffset = Random.Range (-3f, 3f);
 
-						select = Random.Range(0,10);
-						if(select < 3){
+						select = Random.Range (0, 10);
+						if (select < 3) {
 							// long wall
 							objectType = wallShortPrefab;
 
-						} else if( select < 5) {
+						} else if (select < 5) {
 							// U
 							objectType = wallUPrefab;
 						} else if (select < 8) {
@@ -98,15 +104,15 @@ public class Level : MonoBehaviour {
 						xcoord = j * groundTileSize + k + xOffset;
 						ycoord = i * groundTileSize - l + yOffset + screenOffset;
 
-						if(Mathf.Abs(xcoord) < 10 && Mathf.Abs(ycoord) < 10){
-							Debug.Log("Negeneruji!!!!!!" + "x: " + xcoord + "y: " + ycoord);
+						if (Mathf.Abs (xcoord) < 10 && Mathf.Abs (ycoord) < 10) {
+							Debug.Log ("Negeneruji!!!!!!" + "x: " + xcoord + "y: " + ycoord);
 							continue;
 						}
 
 						// generate wall
 						GameObject wall = Instantiate (objectType, new Vector3 (xcoord, 
-						                                                        ycoord, 10), 
-						                               Quaternion.Euler (0, 0, (Random.Range(0, 359)))) as GameObject;
+						                                                        ycoord, 8), 
+						                               Quaternion.Euler (0, 0, (Random.Range (0, 359)))) as GameObject;
 						//wall.transform.parent = transform;
 					}
 				}
@@ -116,7 +122,8 @@ public class Level : MonoBehaviour {
 		
 	}
 
-	void CreatePowerUps(){
+	void CreatePowerUps ()
+	{
 		int screenOffset = 17;
 		
 		
@@ -132,14 +139,14 @@ public class Level : MonoBehaviour {
 				float xcoord = 0;
 				float ycoord = 0; 
 				
-				for(int k=0; k<=100; k+=15){
-					for(int l=0; l<=40; l+=15){
+				for (int k=0; k<=100; k+=15) {
+					for (int l=0; l<=40; l+=15) {
 						
-						xOffset = Random.Range(-6f, 6f);
-						yOffset = Random.Range(-6f, 6f);
+						xOffset = Random.Range (-6f, 6f);
+						yOffset = Random.Range (-6f, 6f);
 						
-						select = Random.Range(0,10);
-						if(select < 7){
+						select = Random.Range (0, 10);
+						if (select < 7) {
 							// camera powerup
 							powerupType = PowerupManager.E_PowerupType.PowerUpCamera;
 
@@ -151,14 +158,14 @@ public class Level : MonoBehaviour {
 						xcoord = j * groundTileSize + k + xOffset;
 						ycoord = i * groundTileSize - l + yOffset + screenOffset;
 						
-						if(Mathf.Abs(xcoord) < 5 && Mathf.Abs(ycoord) < 5){
-							Debug.Log("Negeneruji!!!!!!" + "x: " + xcoord + "y: " + ycoord);
+						if (Mathf.Abs (xcoord) < 5 && Mathf.Abs (ycoord) < 5) {
+							Debug.Log ("Negeneruji!!!!!!" + "x: " + xcoord + "y: " + ycoord);
 							continue;
 						}
 						
 						// generate powerup
-						Game.Instance.PowerupManager.GeneratePowerup(powerupType, 
-						                                             new Vector3 (xcoord,ycoord, 10), Quaternion.identity);
+						Game.Instance.PowerupManager.GeneratePowerup (powerupType, 
+						                                             new Vector3 (xcoord, ycoord, 10), Quaternion.identity);
 						//wall.transform.parent = transform;
 					}
 				}
@@ -170,39 +177,42 @@ public class Level : MonoBehaviour {
 	
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	    
 	}
 
 
-	void createWallAtXY(int x, int y, int rotMin, int rotMax){
+	void createWallAtXY (int x, int y, int rotMin, int rotMax)
+	{
 
-		GameObject wall = Instantiate(wallLongPrefab, new Vector3(x, Random.Range(-55, -10), 0), 
-		            Quaternion.Euler (0, 0, (Random.Range(rotMin, rotMax)))) as GameObject;
+		GameObject wall = Instantiate (wallLongPrefab, new Vector3 (x, Random.Range (-55, -10), 0), 
+		            Quaternion.Euler (0, 0, (Random.Range (rotMin, rotMax)))) as GameObject;
         
-        //wall.transform.parent = transform;
-        wall.SetActive(true);
+		//wall.transform.parent = transform;
+		wall.SetActive (true);
 	}
 
 
 
 
-    void SetTint(Color color) {
-        foreach (SpriteRenderer sr in m_groundSprites) {
-            sr.color = color;
-        }
-    }
+	void SetTint (Color color)
+	{
+		foreach (SpriteRenderer sr in m_groundSprites) {
+			sr.color = color;
+		}
+	}
 
-    IEnumerator _TintRepeated() {
-        for (;;) {
-            if (Game.Instance.PowerupManager.IsPowerupActive(PowerupManager.E_PowerupType.PowerUpCamera)) {
-                float c = Random.Range(0.2f, 1f);
-                SetTint(new Color(c, c, c));
-            }
-            else {
-                SetTint(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
-            }
-            yield return new WaitForSeconds(tintInterval);
-        }
-    }
+	IEnumerator _TintRepeated ()
+	{
+		for (;;) {
+			if (Game.Instance.PowerupManager.IsPowerupActive (PowerupManager.E_PowerupType.PowerUpCamera)) {
+				float c = Random.Range (0.2f, 1f);
+				SetTint (new Color (c, c, c));
+			} else {
+				SetTint (new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f)));
+			}
+			yield return new WaitForSeconds (tintInterval);
+		}
+	}
 }
