c70446298068beec487b8f4e830cd25a8aeb1d81
p4jos <pavelcsoka@dhcps132.fit.vutbr.cz>
2015-03-21 01:06:27 +0100

    added blood marks sprites, leaderboard hidden

---
+++ Assets/Scripts/BloodMarks.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodMarks : MonoBehaviour
+{
+
+	public GameObject bloodHelp;
+	public GameObject bloodStain;
+	public GameObject bloodDie;
+
+	// Use this for initialization
+	void Start ()
+	{	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+	}
+}
